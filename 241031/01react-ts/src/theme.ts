aeaaabe26849bc6ee4ef0c5718e3b09fdfc25a0d
daeundan <ekdms534@naver.com>
2024-10-31 12:08:07 +0900

    Typescript Settings

--- 241031/01react-ts/src/theme.ts
+++ 241031/01react-ts/src/theme.ts
@@ -3,6 +3,6 @@ import { DefaultTheme } from "styled-components/dist/types";
 export const defaultTheme = {
   bgColor: "#2f3640",
   textColor: "#fff",
-  accentColor: "#4cd137",
+  accentsColor: "#4cd137",
   cardBgColor: "transparent",
 };
